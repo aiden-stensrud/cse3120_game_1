b4a269530ef19a875589889627b2bc33ee7fe567
Aiden Stensrud <aiden.stensrud@gmail.com>
2026-04-02 02:13:17 -0400

    moved words variables to their own file, rewrote assembler to produce hangman.exe

--- cse game 1/src/assemble.bat
+++ cse game 1/src/assemble.bat
@@ -1,41 +1,38 @@
 @ECHO OFF
-rem File: asm_CSE3120.bat
-rem Author: Aiden Stensrud, 2026
-SET IRVINE=..\Irvine
-SET FILEBASE=%~n1
-echo Handling Source: %FILEBASE%
+rem File: assemble.bat
+rem Hardcoded build for main.asm + words.asm → hangman.exe
+
 setlocal
 
-rem You may use quotes for the whole parameter
-set "PATH=C:\Program Files (x86);...;%PATH%"
+SET IRVINE=..\Irvine
 
-rem Or you should use quotes only for special
-rem characters. Avoid final undesired spaces...
-set PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin;C:\Program Files (x86)\Windows Kits\8.1\bin\x86;;C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\tools;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\ide;C:\Program Files (x86)\HTML Help Workshop;;C:\Program Files (x86)\MSBuild\14.0\bin\;C:\windows\Microsoft.NET\Framework\v4.0.30319\;C:\windows\SysWow64;;C:\windows\system32;C:\windows;C:\windows\System32\Wbem;C:\windows\System32\WindowsPowerShell\v1.0\;C:\windows\System32\OpenSSH\;C:\Program Files (x86)\Windows Kits\8.1\Windows Performance Toolkit\;;;
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin;C:\Program Files (x86)\Windows Kits\8.1\bin\x86;C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\tools;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\ide;C:\Program Files (x86)\HTML Help Workshop;C:\Program Files (x86)\MSBuild\14.0\bin\;C:\windows\Microsoft.NET\Framework\v4.0.30319\;C:\windows\SysWow64;C:\windows\system32;C:\windows;C:\windows\System32\Wbem;C:\windows\System32\WindowsPowerShell\v1.0\;C:\windows\System32\OpenSSH\;C:\Program Files (x86)\Windows Kits\8.1\Windows Performance Toolkit\;%PATH%
 
+set LIB=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\lib;C:\Program Files (x86)\Windows Kits\10\lib\10.0.10240.0\ucrt\x86;C:\Program Files (x86)\Windows Kits\8.1\lib\winv6.3\um\x86;C:\Program Files (x86)\Windows Kits\NETFXSDK\4.6.1\Lib\um\x86
 
-set LIB=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib;;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\lib;;C:\Program Files (x86)\Windows Kits\10\lib\10.0.10240.0\ucrt\x86;;;C:\Program Files (x86)\Windows Kits\8.1\lib\winv6.3\um\x86;;C:\Program Files (x86)\Windows Kits\NETFXSDK\4.6.1\Lib\um\x86
+set LIBPATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\lib;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib
 
+set INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\include;C:\Program Files (x86)\Windows Kits\10\Include\10.0.10240.0\ucrt;C:\Program Files (x86)\Windows Kits\8.1\Include\um;C:\Program Files (x86)\Windows Kits\8.1\Include\shared;C:\Program Files (x86)\Windows Kits\8.1\Include\winrt
 
-set LIBPATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\lib;;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib;
+echo Assembling main.asm...
+ml.exe /c /nologo /Sg /Zi /Fo"main.obj" /Fl"main.lst" /I "%IRVINE%" /W3 /errorReport:prompt /Tamain.asm
+IF ERRORLEVEL 1 GOTO build_fail
 
+echo Assembling words.asm...
+ml.exe /c /nologo /Sg /Zi /Fo"words.obj" /Fl"words.lst" /I "%IRVINE%" /W3 /errorReport:prompt /Tawords.asm
+IF ERRORLEVEL 1 GOTO build_fail
 
-set INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include;;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\include;;C:\Program Files (x86)\Windows Kits\10\Include\10.0.10240.0\ucrt;;;C:\Program Files (x86)\Windows Kits\8.1\Include\um;C:\Program Files (x86)\Windows Kits\8.1\Include\shared;C:\Program Files (x86)\Windows Kits\8.1\Include\winrt;;
+echo Linking hangman.exe...
+link.exe /ERRORREPORT:PROMPT /OUT:"hangman.exe" /NOLOGO /LIBPATH:%IRVINE% main.obj words.obj user32.lib irvine32.lib kernel32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /MANIFEST /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /manifest:embed /DEBUG /SUBSYSTEM:CONSOLE /TLBID:1 /DYNAMICBASE:NO /MACHINE:X86 /SAFESEH:NO
+IF ERRORLEVEL 1 GOTO build_fail
 
-rem EXTERNAL_INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\include;;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\atlmfc\include;;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\VS\include;;C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt;;;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\VS\UnitTest\include;C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um;C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\shared;C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\winrt;C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\cppwinrt;C:\Program Files (x86)\Windows Kits\NETFXSDK\4.8\Include\um;
+echo.
+echo Build successful → hangman.exe
+GOTO end
 
+:build_fail
+echo.
+echo Build failed.
 
-rem ml.exe /c /nologo /Sg /Zi /Fo"%FILEBASE%.obj"\
-rem /Fl"%FILEBASE%.lst" /I "%IRVINE%" /W3 \
-rem /errorReport:prompt /Ta%FILEBASE%.asm
-
-
-FOR %%F IN (%*) DO (
-echo Handling %%~nF
-ml.exe /c /nologo /Sg /Zi /Fo"%%~nF.obj" /Fl"%%~nF.lst" /I "%IRVINE%" /W3 /errorReport:prompt /Ta%%~nF.asm
-)
-
-link.exe /ERRORREPORT:PROMPT /OUT:"%FILEBASE%.exe" /NOLOGO /LIBPATH:%IRVINE% user32.lib irvine32.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /MANIFEST /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /manifest:embed /DEBUG /SUBSYSTEM:CONSOLE /TLBID:1 /DYNAMICBASE:NO /MACHINE:X86 /SAFESEH:NO %FILEBASE%.obj
-
+:end
 endlocal
-
---
+++ cse game 1/src/words.asm
@@ -0,0 +1,38 @@
+.386
+.model flat, stdcall
+option casemap:none
+
+PUBLIC words
+PUBLIC wordCount
+
+.data
+
+word1  BYTE "cat", 0
+word2  BYTE "dog", 0
+word3  BYTE "four", 0
+word4  BYTE "goat", 0
+word5  BYTE "tiger", 0
+word6  BYTE "world", 0
+word7  BYTE "pizza", 0
+word8  BYTE "water", 0
+word9  BYTE "planet", 0
+word10 BYTE "orange", 0
+word11 BYTE "cabana", 0
+word12 BYTE "chair", 0
+word13 BYTE "pencil", 0
+word14 BYTE "school", 0
+word15 BYTE "polish", 0
+word16 BYTE "elegance", 0
+word17 BYTE "abandon", 0
+word18 BYTE "bread", 0
+word19 BYTE "zebra", 0
+word20 BYTE "window", 0
+
+words DWORD OFFSET word1, OFFSET word2, OFFSET word3, OFFSET word4, OFFSET word5
+      DWORD OFFSET word6, OFFSET word7, OFFSET word8, OFFSET word9, OFFSET word10
+      DWORD OFFSET word11, OFFSET word12, OFFSET word13, OFFSET word14, OFFSET word15
+      DWORD OFFSET word16, OFFSET word17, OFFSET word18, OFFSET word19, OFFSET word20
+
+wordCount DWORD 20
+
+END
