35761afdc64ed18c5c619df7450ee837d82a5144
Aiden Stensrud <aiden.stensrud@gmail.com>
2026-04-01 22:00:29 -0400

    added initialization of un-revealed word

--- cse game 1/src/main.asm
+++ cse game 1/src/main.asm
@@ -8,6 +8,7 @@ INCLUDE Irvine32.inc
 ExitProcess proto,dwExitCode:dword
 .data
 guess_word BYTE "hello",0
+revealed_word BYTE "_____",0
 hangman BYTE \
 " +---+",0Dh,0Ah,\
 " |   |",0Dh,0Ah,\
@@ -25,8 +26,28 @@ main proc
 	; prints the inital hangman character
 	mov edx, OFFSET hangman
     call WriteString
-    call Crlf			
+    call Crlf
+	mov edx, OFFSET revealed_word
+    call WriteString
+    call Crlf		
 
 	invoke ExitProcess,0
 main endp
+
+InitRevealedWord PROC ; initialize blank un-revealed word
+    mov esi, OFFSET guess_word      ; source
+    mov edi, OFFSET revealed_word   ; destination
+buildLoop:
+    mov al, [esi]
+    cmp al, 0
+    je done
+    mov BYTE PTR [edi], '_'         ; replace with underscore
+    inc esi
+    inc edi
+    jmp buildLoop
+done:
+    mov BYTE PTR [edi], 0           ; null terminate
+    ret
+InitRevealedWord ENDP
+
 end main
