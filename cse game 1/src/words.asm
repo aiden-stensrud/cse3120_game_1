920d8566687775cf4c0f1a9543a38a78940630a2
Aiden Stensrud <aiden.stensrud@gmail.com>
2026-04-25 14:51:18 -0400

    added variables for new means of retrieving word data: reading them through a txt file at runtime instead of during assemble time
    note: irvine32 functions were not fully replaced as they do not work at the moment. they will be fixed later in developement, but for now irvine32 variants will be used for testing other features.

--- cse game 1/src/words.asm
+++ cse game 1/src/words.asm
@@ -2,8 +2,16 @@
 .model flat, stdcall
 option casemap:none
 
+CreateFileA PROTO :DWORD,:DWORD,:DWORD,:DWORD,:DWORD,:DWORD,:DWORD
+ReadFile    PROTO :DWORD,:DWORD,:DWORD,:DWORD,:DWORD
+CloseHandle PROTO :DWORD
+GENERIC_READ          EQU 80000000h
+OPEN_EXISTING         EQU 3
+FILE_ATTRIBUTE_NORMAL EQU 80h
+
 PUBLIC words
 PUBLIC wordCount
+PUBLIC LoadWords
 
 .data
 
@@ -22,7 +30,7 @@ word12 BYTE "chair", 0
 word13 BYTE "pencil", 0
 word14 BYTE "school", 0
 word15 BYTE "polish", 0
-word16 BYTE "elegance", 0
+word16 BYTE "i love very long sentences", 0
 word17 BYTE "abandon", 0
 word18 BYTE "bread", 0
 word19 BYTE "zebra", 0
@@ -34,5 +42,11 @@ words DWORD OFFSET word1, OFFSET word2, OFFSET word3, OFFSET word4, OFFSET word5
       DWORD OFFSET word16, OFFSET word17, OFFSET word18, OFFSET word19, OFFSET word20
 
 wordCount DWORD 20
+
+.code
+
+LoadWords PROC
+	ret
+LoadWords ENDP
 
 END
