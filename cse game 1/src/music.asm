93814a2e95a717bcbb41a40a6ac54e274541120e
Aiden Stensrud <aiden.stensrud@gmail.com>
2026-04-27 21:18:15 -0400

    added procedure to play game theme as well as game theme file

--- cse game 1/src/music.asm
+++ cse game 1/src/music.asm
@@ -1,6 +1,17 @@
 INCLUDE Irvine32.inc
 .386
 .stack 4096
+includelib winmm.lib
+PlaySoundA PROTO :DWORD, :DWORD, :DWORD
 .data
+
+game_theme BYTE "Music\\game.wav",0
+
 .code
+
+PlayGameTheme PROC
+	INVOKE PlaySoundA, OFFSET game_theme, 0, 8h
+	ret
+PlayGameTheme ENDP
+
 end
